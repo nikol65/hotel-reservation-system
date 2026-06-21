2fa3370091e061fa11237a76506b2ed1c187b73b
Nikol Dimitrova <nikydimitrova@abv.bg>
2026-06-21 14:07:33 +0300

    Implement profile statistics

--- src/main/java/bg/softuni/hotel_reservation_system/web/UserController.java
+++ src/main/java/bg/softuni/hotel_reservation_system/web/UserController.java
@@ -1,6 +1,8 @@
 package bg.softuni.hotel_reservation_system.web;
 
 import bg.softuni.hotel_reservation_system.model.dto.user.UserDto;
+import bg.softuni.hotel_reservation_system.repository.ReservationRepository;
+import bg.softuni.hotel_reservation_system.service.PaymentService;
 import bg.softuni.hotel_reservation_system.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,26 +10,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Controller
 public class UserController {
 
     private final UserService userService;
+    private final ReservationRepository reservationRepository;
+    private final PaymentService paymentService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ReservationRepository reservationRepository, PaymentService paymentService) {
         this.userService = userService;
+        this.reservationRepository = reservationRepository;
+        this.paymentService = paymentService;
     }
 
     @GetMapping("/profile")
     public ModelAndView profile(HttpSession session) {
         ModelAndView modelAndView = new ModelAndView("profile");
+
         UUID id = (UUID) session.getAttribute("userId");
-        if (id != null) {
-            modelAndView.addObject("user", userService.findUserById(id));
-        }else {
+
+        if (id == null) {
             return new ModelAndView("redirect:/login");
         }
+
+        UserDto user = userService.findUserById(id);
+
+        long totalReservations = reservationRepository.countByUserId(id);
+        BigDecimal totalSpent = paymentService.calculateTotalSpentByUser(id);
+
+        String loyaltyStatus = "Standard";
+
+        if (totalSpent.compareTo(BigDecimal.valueOf(1000)) >= 0) {
+            loyaltyStatus = "Gold";
+        } else if (totalSpent.compareTo(BigDecimal.valueOf(500)) >= 0) {
+            loyaltyStatus = "Silver";
+        }
+
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("totalReservations", totalReservations);
+        modelAndView.addObject("totalSpent", totalSpent);
+        modelAndView.addObject("loyaltyStatus", loyaltyStatus);
+
         return modelAndView;
     }
 }
