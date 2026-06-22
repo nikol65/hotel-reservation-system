52fd66545eecb26d6d896adea9ba30da0464a7a7
Nikol Dimitrova <nikydimitrova@abv.bg>
2026-06-22 21:18:11 +0300

    Implement user profile editing

--- src/main/java/bg/softuni/hotel_reservation_system/web/UserController.java
+++ src/main/java/bg/softuni/hotel_reservation_system/web/UserController.java
@@ -1,14 +1,19 @@
 package bg.softuni.hotel_reservation_system.web;
 
+import bg.softuni.hotel_reservation_system.model.dto.user.EditUserRequest;
 import bg.softuni.hotel_reservation_system.model.dto.user.UserDto;
 import bg.softuni.hotel_reservation_system.repository.ReservationRepository;
 import bg.softuni.hotel_reservation_system.service.PaymentService;
 import bg.softuni.hotel_reservation_system.service.UserService;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -20,7 +25,9 @@ public class UserController {
     private final ReservationRepository reservationRepository;
     private final PaymentService paymentService;
 
-    public UserController(UserService userService, ReservationRepository reservationRepository, PaymentService paymentService) {
+    public UserController(UserService userService,
+                          ReservationRepository reservationRepository,
+                          PaymentService paymentService) {
         this.userService = userService;
         this.reservationRepository = reservationRepository;
         this.paymentService = paymentService;
@@ -28,8 +35,6 @@ public class UserController {
 
     @GetMapping("/profile")
     public ModelAndView profile(HttpSession session) {
-        ModelAndView modelAndView = new ModelAndView("profile");
-
         UUID id = (UUID) session.getAttribute("userId");
 
         if (id == null) {
@@ -38,23 +43,77 @@ public class UserController {
 
         UserDto user = userService.findUserById(id);
 
-        long totalReservations = reservationRepository.countByUserId(id);
-        BigDecimal totalSpent = paymentService.calculateTotalSpentByUser(id);
-
-        String loyaltyStatus = "Standard";
-
-        if (totalSpent.compareTo(BigDecimal.valueOf(1000)) >= 0) {
-            loyaltyStatus = "Gold";
-        } else if (totalSpent.compareTo(BigDecimal.valueOf(500)) >= 0) {
-            loyaltyStatus = "Silver";
-        }
-
+        ModelAndView modelAndView = new ModelAndView("profile");
         modelAndView.addObject("user", user);
-        modelAndView.addObject("totalReservations", totalReservations);
-        modelAndView.addObject("totalSpent", totalSpent);
-        modelAndView.addObject("loyaltyStatus", loyaltyStatus);
+
+        if ("USER".equals(user.getRole().toString())) {
+            long totalReservations = reservationRepository.countByUserId(id);
+            BigDecimal totalSpent = paymentService.calculateTotalSpentByUser(id);
+
+            String loyaltyStatus = "Standard";
+
+            if (totalSpent.compareTo(BigDecimal.valueOf(1000)) >= 0) {
+                loyaltyStatus = "Gold";
+            } else if (totalSpent.compareTo(BigDecimal.valueOf(500)) >= 0) {
+                loyaltyStatus = "Silver";
+            }
+
+            modelAndView.addObject("totalReservations", totalReservations);
+            modelAndView.addObject("totalSpent", totalSpent);
+            modelAndView.addObject("loyaltyStatus", loyaltyStatus);
+        }
 
         return modelAndView;
     }
 
+    @GetMapping("/profile/edit")
+    public ModelAndView editProfile(HttpSession session) {
+        UUID id = (UUID) session.getAttribute("userId");
+
+        if (id == null) {
+            return new ModelAndView("redirect:/login");
+        }
+
+        UserDto user = userService.findUserById(id);
+
+        EditUserRequest editUserRequest = EditUserRequest.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
+
+        ModelAndView modelAndView = new ModelAndView("edit-profile");
+        modelAndView.addObject("editUserRequest", editUserRequest);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/profile/edit")
+    public ModelAndView editProfile(@Valid @ModelAttribute("editUserRequest") EditUserRequest editUserRequest,
+                                    BindingResult bindingResult,
+                                    HttpSession session,
+                                    RedirectAttributes redirectAttributes) {
+        UUID id = (UUID) session.getAttribute("userId");
+
+        if (id == null) {
+            return new ModelAndView("redirect:/login");
+        }
+
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("edit-profile");
+        }
+
+        try {
+            userService.updateProfile(id, editUserRequest);
+        } catch (RuntimeException e) {
+            ModelAndView modelAndView = new ModelAndView("edit-profile");
+            modelAndView.addObject("editUserRequest", editUserRequest);
+            modelAndView.addObject("errorMessage", e.getMessage());
+            return modelAndView;
+        }
+
+        redirectAttributes.addFlashAttribute("successMessage", "Profile updated successfully.");
+        return new ModelAndView("redirect:/profile");
+    }
 }
